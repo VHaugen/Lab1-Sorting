982506dff64e7c08534a441bf4fcf41a9053927f
VHaugen <vicke00@live.com>
2020-04-03 11:02:22 +0200

    first commit

---
+++ src/Lab1.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+
+public class Lab1 {
+
+    public void mergeSort()
+    {
+
+
+    }
+
+    public void insertionSort()
+    {
+
+
+    }
+    public void quickSort()
+    {
+
+
+    }
+
+}
